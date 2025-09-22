20642caa8a6963eed5fe7cf7d1b22424d7a3a485
Fabiox7778 <fabiorabello08@gmail.com>
2025-09-22 14:15:23 -0300

    Create add

--- src/controllers/imoveisControlers.js
+++ src/controllers/imoveisControlers.js
@@ -27,4 +27,101 @@ const getImoveisByld = (req, res) => {
     });
 };
 
-export { getAllImoveis, getImoveisByld };
+const createImovel = (req, res) => {
+    const { tipo, endereco, area, quartos, preco, disponivel, proprietario } = req.body;
+
+    const tiposDeImoveis = ["Apartamento", "Casa de Condomínio", "Terreno", "Kitnet", "Chácara", "Sala Comercial", "Loft", "Casa de Vila", "Apartamento Duplex", "Galpão Industrial", "Apartamento na Praia", "Casa Térrea", "Studio", "Fazenda", "Apartamento Garden", "Sobrado", "Terreno Comercial", "Apartamento 1 por andar", "Casa de Campo", "Flat/Aparthotel", "Prédio Comercial", "Ponto Comercial (Loja)", "Cobertura Penthouse", "Sítio", "Apartamento Compacto"];
+
+    if (!tipo) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'tipo' é obrigatório"
+        });
+    }
+
+    if (!endereco) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'endereco' é obrigatório"
+        });
+    }
+
+    if (!area) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'area' é obrigatório"
+        });
+    }
+
+    if (!quartos) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'quartos' é obrigatório"
+        });
+    }
+
+    if (!preco) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'preco' é obrigatório"
+        });
+    }
+
+    if (!disponivel) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'disponivel' é obrigatório"
+        });
+    }
+
+    if (!proprietario) {
+        return res.status(400).json({
+            success: false,
+            message: "O campo 'proprietario' é obrigatório"
+        });
+    }
+
+    if (area > 0) {
+        return res.status(400).json({
+            success: false,
+            message: "Área deve ser um número maior que 0 metros quadrados"
+        })
+    } 
+
+    if (preco > 0) {
+        return res.status(400).json({
+            success: false,
+            message: "Preço deve ser um número maior que 0"
+        })
+    }
+
+    if (!tiposDeImoveis.includes(tipo)) {
+        return res.status(400).json({
+            success: false,
+            message: `O tipo "${tipo}" não é válido. Tipos permitidos: ${tiposDeImoveis.join(", ")}.`
+        });
+    };
+
+
+    const novoImovel = {
+        id: imoveis.length + 1,
+        tipo: tipo,
+        endereco,
+        area,
+        quartos,
+        preco,
+        disponivel,
+        proprietario
+    };
+
+    imoveis.push(novoImovel);
+
+    res.status(201).json({
+        success: true,
+        message: "Novo imóvel Cadastrada com sucesso",
+        data: novoImovel
+    });
+
+};
+
+export { getAllImoveis, getImoveisByld, createImovel };
---
+++ src/routes/imoveisRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getAllImoveis, getImoveisByld, createImovel } from "../controllers/imoveisControlers.js";
+
+const router = express.Router();
+
+//Filtros implementares:
+
+
+//Filtros CRUD:
+router.get("/", getAllImoveis);
+router.get("/:id", getImoveisByld);
+router.post("/", createImovel);
+//router.delete("/:id", deleteImovel);
+//router.put("/:id", updateImovel);
+
+export default router; 
